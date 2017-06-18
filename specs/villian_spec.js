2b8ea4ff12f5f2134ae9a05400d5db60b983b83d
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-06-18 18:48:31 +0100

    test villian can attack with various moves and double the damage when superCharged

--- specs/villian_spec.js
+++ specs/villian_spec.js
@@ -14,5 +14,27 @@ describe('Test Villian', function(){
 
   it("Villian has a name", function () {
     assert.strictEqual(drx.name, "Doctor X");
+  });
+
+  it("Villian can damage hero with 10 health point when punching", function () {
+    drx.attack( superman, "Punch");
+    assert.strictEqual( superman.health, 90);
+  });
+
+  it("Villian can damage hero with 30 health point when FulNeckSwing", function () {
+    drx.attack( superman, "FulNeckSwing");
+    assert.strictEqual( superman.health, 70);
+  });
+
+  it("Villian can supercharge", function () {
+    drx.superCharge();
+    assert.strictEqual(drx.isSuperCharged, true);
+  });
+
+  it("Villian double damage when supercharged", function () {
+    drx.superCharge();
+    drx.attack( superman, "FulNeckSwing" );
+    assert.strictEqual( superman.health, 40 );
   })
+
 })
--- villian.js
+++ villian.js
@@ -1,5 +1,29 @@
 var Villian = function( name ){
   this.name = name;
+  this.moves = {
+    Punch: 10,
+    Kick: 15,
+    FulNeckSwing: 30,
+    SideStepElbow: 20,
+    OrbitalHeel: 21,
+    GutPunch: 18,
+    KneeCrusher: 42
+  };
+  this.isSuperCharged = false;
+}
+
+Villian.prototype = {
+  attack: function ( hero , move ) {
+    var superChargeFactor = 1;
+    if( this.isSuperCharged ){
+      superChargeFactor = 2;
+    }
+    hero.health -= this.moves[move] * superChargeFactor;
+  },
+
+  superCharge: function () {
+    this.isSuperCharged = true;
+  }
 }
 
 module.exports = Villian;
