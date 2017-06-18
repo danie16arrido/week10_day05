2f4ea924f956a226aebff39b79a642884c893373
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-06-18 12:29:40 +0100

    pass test tasks can have urgency level

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -7,7 +7,7 @@ describe('Test Task', function(){
   var myTask;
 
   beforeEach(function () {
-    myTask = new Task("Kill Monsters", 10);
+    myTask = new Task("Kill Monsters", 10, 5);
   })
 
   it("task has a difficulty level", function () {
@@ -17,6 +17,15 @@ describe('Test Task', function(){
   it("can set task difficulty", function () {
     myTask.setDifficulty(9);
     assert.strictEqual(myTask.difficulty, 9);
+  });
+
+  it("task has urgency level", function () {
+    assert.strictEqual(myTask.defcon, 5);
+  });
+
+  it("can set defcon to new value", function () {
+    myTask.setDefcon(4);
+    assert.strictEqual(myTask.defcon, 4);
   });
 
 })
--- task.js
+++ task.js
@@ -1,12 +1,19 @@
-var Task = function ( name, difficulty) {
+var Task = function ( name, difficulty, defcon) {
   this.name = name;
   this.difficulty = difficulty;
+  this.defcon = defcon;
+
 }
 
 Task.prototype = {
   setDifficulty: function ( difficulty ) {
     this.difficulty = difficulty;
+  },
+
+  setDefcon: function ( defcon ) {
+    this.defcon = defcon;
   }
+
 };
 
 module.exports = Task;
