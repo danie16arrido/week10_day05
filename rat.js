7aa41ca9e9f382f22d63aca4045b4c0caa0d90f3
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-06-18 16:25:10 +0100

    test Rat can poison food

--- rat.js
+++ rat.js
@@ -2,4 +2,10 @@ var Rat = function ( name ) {
   this.name = name;
 }
 
+Rat.prototype = {
+  touchFood: function ( food ) {
+    food.setPoisonous();
+  }
+}
+
 module.exports = Rat;
--- specs/rat_spec.js
+++ specs/rat_spec.js
@@ -1,16 +1,25 @@
 var Rat = require('../rat.js');
+var Food = require('../food.js');
 var assert = require('assert');
+
 
 
 describe('Test Rat', function(){
 
   var myRat;
+  var food;
 
   beforeEach(function () {
     myRat = new Rat("Raton");
+    food = new Food("Apple", 3 );
   })
 
   it("rat has a name", function () {
     assert.strictEqual(myRat.name, "Raton");
   });
+
+  it("rat can contaminate food", function () {
+    myRat.touchFood( food );
+    assert.strictEqual( true, food.isPoisonous );
+  })
 })
